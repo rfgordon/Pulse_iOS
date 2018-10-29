1aa2a64843a15aaff7de5c396161a9feac808c41
MikeZaki <ezaki.mike@gmail.com>
2018-10-29 15:34:57 -0400

    Moving Initial Development Work To Github

--- Pulse_iOS/ViewController.swift
+++ Pulse_iOS/ViewController.swift
@@ -7,9 +7,17 @@
 //
 
 import UIKit
+import Charts
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ppgOrganDelegate {
 
+  @IBOutlet weak var lineChartView: LineChartView!
+  
+  private let captureOrgan: PulseCaptureOrgan = PulseCaptureOrgan()
+  private lazy var ppgOrgan: PulsePPGOrgan = {
+    return PulsePPGOrgan(captureOrgan: self.captureOrgan)
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,29 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
-
+  override func viewWillAppear(_ animated: Bool) {
+    ppgOrgan.delegate = self
+    ppgOrgan.beat()
+  }
+  
+  func ppgOrganDidBeat(with values: [ChartDataEntry]) {
+    
+    let line1 = LineChartDataSet(values: values, label: "PPG")
+    line1.colors = [UIColor.red]
+    line1.drawCirclesEnabled = false
+    line1.mode = .cubicBezier
+    line1.cubicIntensity = 0.2
+    
+    let data = LineChartData()
+    data.addDataSet(line1)
+    data.setValueTextColor(NSUIColor.clear)
+    
+    lineChartView.data = data
+    lineChartView.setScaleEnabled(false)
+    lineChartView.leftAxis.minWidth = 1.0
+    
+    
+    lineChartView.chartDescription?.text = "My Awesome Chart"
+  }
 }
 
